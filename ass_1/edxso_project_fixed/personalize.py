d89735f60f9f6f6a8cd4ec067a3ef786de838ab3
Deepanshu Sharma <deepanshusharma0846@gmail.com>
2026-08-26 18:41:55 +0530

    removed unused folders

--- ass_1/edxso_project_fixed/personalize.py
+++
@@ -1,9 +0,0 @@
-import sys
-import os
-
-sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)))
-
-from ass_1.src.personalization.personalize import run_personalize
-
-if __name__ == "__main__":
-    run_personalize()
